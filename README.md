193c127d1fe86b3131d02a776280535d0913794f
Natapon <w.natapon@gmail.com>
2023-07-29 11:54:10 +0200

    add observation projection

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 ## To-Do
 - [ ] Function that map dates to timeseries, cluster them, and plot the cluster group
-- [ ] PCA/TSNE/UMAP Analysis of different energy system KPIs link to regions
+- [x] PCA/TSNE/UMAP Analysis of different energy system KPIs link to regions
 - [x] Shapley value analysis
-- [ ]
--- src/ESAAnalytics.jl
+++ src/ESAAnalytics.jl
@@ -1,10 +1,46 @@
 module ESAAnalytics
 
 # Import dependencies =========================================================
+import MultivariateStats
+import UMAP
 
 # Declare export ==============================================================
-
+export project_pca, project_umap
 export shapleyvalueanalysis
+
+"""
+    project_pca(data::Matrix; dims=2)
+
+Project `data`, a matrix of (features x observation), into a lower dimension space using the PCA method
+
+# Keyword Arguments
+- `dims` : number of dimensions of the projections
+"""
+function project_pca(data::Matrix; dims=2)
+    model = MultivariateStats.fit(MultivariateStats.PCA, data; maxoutdim=dims)
+    explainedvar = round(model.tprinvar / model.tvar, digits=4)
+    @info "Explained variance $(explainedvar)"
+    projecteddata = MultivariateStats.predict(model, data)
+    return projecteddata
+end
+
+"""
+    project_umap(data::Matrix; dims=2, kwargs...)
+
+Project `data`, a matrix of (features x observation), into a lower dimension space using the UMAP method
+
+# Keyword Arguments
+- `dims` : number of dimensions of the projections
+- `n_neighbors::Int=15` : This controls how many neighbors around each point are considered to be part of its local neighborhood. Larger values will result in embeddings that capture more global structure, while smaller values will preserve more local structures.
+- `min_dist::Float=0.1` : This controls the minimum spacing of points in the embedding. Larger values will cause points to be more evenly distributed, while smaller values will preserve more local structure.
+
+See also : [`UMAP.UMAP_`](@ref)
+"""
+function project_umap(data::Matrix; dims=2, kwargs...)
+    model = UMAP.UMAP_(data, dims; kwargs...)
+    projdata = model.embedding
+    return projdata
+end
 
 """
     shapleyvalueanalysis(benefits)
